5d0096d6c709b49a3570fe2ee3e8706aca2c119d
ue70941 <christopher.dickinson@sbb.ch>
2021-03-07 21:38:22 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,124 +1,23 @@
-# Quora-scraper
+# Quora-expander
 
-[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://github.com/banyous/quora-scraper)
+Quora-expander is a command-line application written in Python that can be helpful for automatically expanding articles and comments for any given Quora user.
 
+To run `quora-expander`:
 
-Quora-scraper is a command-line application written in Python that simulates a browser environment to let you scrape Quora rich textual data. You can use one of the three scraping modules to: Find questions that discuss about certain topics (such as Finance, Politics, Tesla or Donald-Trump). Scrape Quora answers related to certain questions, or scrape users profile. Please use it responsibly ! 
-
-## Install
-To use our scraper, please follow the steps below:
-- Install python 3.6 or upper versions.
-- Install the latest version of google-chrome.
-- Install quora-scraper:
+1. Set up python to be run inside a terminal on your local machine
+1. Make sure you have Google Chrome installed on your local machine
+1. Download a version of `chromedriver` that is compatible with your machine and the installed version of Google Chrome and put the binary inside the same directory as the `quora-expander.py` script, possibly replacing any one that was there before.
+1. Run the `quora-expander` with the following command
 
 ```sh
-$ pip install quora-scraper
+$ python -i quora-expander.py [profile_id]
 ```
-To update quora-scraper:
+
+e.g. 
 
 ```sh
-$ pip install quora-scraper --upgrade
+$ python -i quora-expander.py Artem-Boytsov
 ```
 
-Alternatively, you can clone the project and run the following command to install: Make sure you cd into the quora-scraper folder before performing the command below.
-
-```sh
-$  python setup.py install
-```
-
-## Usage
-
-quora-scraper has three scraping modules : ```questions``` ,```answers```,```users```.
-#### 1) Scraping questions URL:
-
-You can scrape questions related to certain topics using ```questions``` command. This module takes as an input a list of topic keywords. Output is a questions_URL file containing the topic's question links. 
-
-Scraping a topic questions can be done as follows:
-
-- a) Use -l parameter + topic keywords list.
-
-    ```sh
-    $ quora-scraper questions -l [finance,politics,Donald-Trump]
-    ```
-
-- b) Use -f parameter + topic keywords file location. (keywords must be line separated inside the file):
-
-    ```sh
-    $ quora-scraper questions -f  topics_file.txt
-    ```
-    
-#### 2) Scraping answers:
-
-Quora answers are scraped using ```answers``` command. This module takes as an input a list of Questions URL. Output is a file of scraped answers (answers.txt). An answer consists of :
-
-Quest-ID | AnswerDate | AnswerAuthor-ID | Quest-tags | Answer-Text 
-
-To scrape answers, use one of the following methods:
-
-- a) Use -l parameter + question URLs list. 
-
-    ```sh
-    $ quora-scraper answers -l [https://www.quora.com/Is-milk-good,https://www.quora.com/Was-Einstein-a-fake-and-a-plagiarist]
-    ```
-
-- b)  Use -f parameter + question URLs file location:
- 
-    ```sh
-    $ quora-scraper answers -f  questions_url.txt
-    ```
- 
-#### 3) Scraping Quora user profile:
-
-You can scrape Quora Users profile using ```users``` command. The users module takes as an input a list of Quora user IDs. The output is UserProfile file containing:
-
-First line :
-UserID | ProfileDescription |ProfileBio | Location | TotalViews |NBAnswers | NBQuestions | NBFollowers |  NBFollowing
-
-Remaining lines (User's answers):
-AnswerDate | QuestionID | AnswerText 
-
-Scraping Users profile can be done as follows:
-
-- a) Use -l parameter + User-IDs list. 
-    ```sh
-    $ quora-scraper users -l [Albert-Einstein-195,Jackie-Chan-8]
-    ```
-   
-- b)  Use -f parameter + User-IDs file. 
-
-    ```sh
-    $ quora-scraper users -f quora_username_file.txt
-    ```
-
-### Notes
-a) Input files must be line separated.
-
-b) Output files fields are tab separated.
-
-c) You can add a list/line index parameter In order to start the scraping from that index. The code below will start scraping from "physics" keyword:
-    ```sh
-    $ quora-scraper questions -l [finance,politics,tech,physics,life,sports]  -i 3
-    ```
-
-d) Quora website puts limit on the number of questions accessible on a topic page. Thus, even if a topic has a large number of questions (ex: 100k), the number scraped questions links will not exceed 2k or 3k questions.
- 
-e) For more help use : 
- ```sh
-    $ quora-scraper --help
- ```
-f) Quora-scraper uses  xpaths and bs4 methods to scrape Quora webpage elements. Since Quora HTML Structure is constantly changing, the code may need modification from time to time. Please feel free to update and contribute to the source-code in order to keep the scraper up-to-date.
-     
-  
-License
-----
-
-This project uses the following license: [MIT]
-
-
-
-
-[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
-
-
-   [MIT]: <https://github.com/banyousr>
-
+**Disclaimer**: If you're an experienced python coder, you'll probably find all kinds of things that are wrong with this script. 
+Feel free to propose improvements :) !
---
+++ quora-expander.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+import argparse
+import time
+from pathlib import Path
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+def connectchrome():
+    options = Options()
+    # options.add_argument('--headless')
+    options.add_argument('log-level=3')
+    options.add_argument("--incognito")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver_path= Path.cwd() / "chromedriver"
+    browser = webdriver.Chrome(executable_path=driver_path, options=options)
+    browser.maximize_window()
+    browser.set_window_position(70, 0, windowHandle ='current')
+    time.sleep(2)
+    return browser
+
+def scrolldown(self):
+    print('scrolling down to get all answers...')
+    last_height = self.page_source
+    loop_scroll=True
+    attempt = 0
+    # scroll down loop until page not changing
+    while loop_scroll:
+        self.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        new_height=self.page_source
+        if new_height == last_height:
+            # in case of not change, we increase the waiting time
+            attempt += 1
+            if attempt==3:# in the n-th attempt we end the scrolling
+                loop_scroll=False
+        else:
+            attempt=0
+        last_height=new_height
+
+def click_on_all(browser, find_by, selector):
+    last_nb_buttons = -1
+    last_nb_buttons_repeated = 0
+    while True:
+        buttons = find_by(selector)
+        nb_buttons = len(buttons)
+
+        print('clicking on', nb_buttons, 'instances of', '"' + selector + '"')
+        for button in buttons:
+            ActionChains(browser).move_to_element(button).click(button).perform()
+            time.sleep(0.5)
+
+        # know when to exit the loop
+        if nb_buttons == 0:
+            break
+        elif nb_buttons == last_nb_buttons:
+            last_nb_buttons_repeated += 1
+            # if we keep seeing the same number of buttons, the buttons are probably broken
+            if last_nb_buttons_repeated == 10:
+                print("aborting. it looks like the remaining", nb_buttons, "buttons may not be responding to clicks.")
+                break
+        else:
+            last_nb_buttons = nb_buttons
+            last_nb_buttons_repeated = 0
+
+
+def show_more_of_articles(browser):
+    click_on_all(browser, browser.find_elements_by_xpath, "//div[contains(text(), '(more)')]")
+
+def view_more_comments(browser):
+    click_on_all(browser, browser.find_elements_by_xpath, "//div[text()[contains(., 'View More Comments')]]")
+
+def view_collapsed_comments(browser):
+    click_on_all(browser, browser.find_elements_by_xpath, "//div[text()[contains(., 'View Collapsed Comments')]]")
+
+def expand_hidden_comments(browser):
+    click_on_all(browser, browser.find_elements_by_css_selector, ".qu-tapHighlight--white .qu-pb--tiny")
+
+def view_more_replies(browser):
+    click_on_all(browser, browser.find_elements_by_xpath, "//div[text()[contains(., 'View More Replies')]]")
+
+def show_more_of_comments(browser):
+    click_on_all(browser, browser.find_elements_by_xpath, "//span[contains(text(), '(more)')]")
+
+def try_again(browser):
+    click_on_all(browser, browser.find_elements_by_xpath, "//div[text()[contains(., 'Try again')]]")
+
+def print_method_name(name):
+    print()
+    print(name + "()")
+
+###############################################################
+
+def msg(name=None):
+    return '''  
+         python -i quora-expander.py [profile_id]
+         
+         Be sure to execute this script by calling 'python' and passing in the '-i' flag so that it starts in interactive mode.
+         
+         Use -h or --help for the full help output.
+        '''
+parser=argparse.ArgumentParser(description='Python (interactive) Quora article and comments expander script', usage=msg())
+parser.add_argument("profile_id", help="The Quora Profile ID you want to visit, e.g. 'Artem-Boytsov'")
+parser.add_argument("-m", "--manual", action="store_true", help="skip all automatic processing and execute commands manually")
+args=parser.parse_args()
+
+url = "https://www.quora.com/profile/" + args.profile_id
+print("opening", url)
+browser = connectchrome()
+browser.get(url)
+time.sleep(2)
+
+nbanswers = browser.find_element_by_xpath("//div[text()[contains(.,'Answers')]]")
+nbanswers = nbanswers.text.strip('Answers').strip().replace(',', '')
+print('Quora Profile has ', nbanswers, ' answers')
+
+###############################################################
+
+def do_scrolldown():
+    print_method_name(do_scrolldown.__name__)
+    scrolldown(browser)
+
+def do_show_more_of_articles():
+    print_method_name(do_show_more_of_articles.__name__)
+    show_more_of_articles(browser)
+
+def do_view_more_comments():
+    print_method_name(do_view_more_comments.__name__)
+    view_more_comments(browser)
+
+def do_view_collapsed_comments():
+    print_method_name(do_view_collapsed_comments.__name__)
+    view_collapsed_comments(browser)
+
+def do_expand_hidden_comments():
+    print_method_name(do_expand_hidden_comments.__name__)
+    expand_hidden_comments(browser)
+
+def do_view_more_replies():
+    print_method_name(do_view_more_replies.__name__)
+    view_more_replies(browser)
+
+def do_show_more_of_comments():
+    print_method_name(do_show_more_of_comments.__name__)
+    show_more_of_comments(browser)
+
+def do_try_again():
+    print_method_name(do_try_again.__name__)
+    try_again(browser)
+
+def do_all():
+    print_method_name(do_all.__name__)
+    print("executing all commands in sequence...")
+    do_scrolldown()
+    do_show_more_of_articles()
+    do_view_more_comments()
+    do_view_collapsed_comments()
+    do_expand_hidden_comments()
+    do_view_more_replies()
+    do_show_more_of_comments()
+    do_try_again()
+
+def attempt_do_all(attempt_nb, total_nb):
+    print()
+    print("attempt", attempt_nb+1, "of", total_nb)
+    do_all()
+
+def print_manual_instructions():
+    print()
+    print("To see which commands you can execute manually, type 'do_' and then Tab (twice) to see the list of commands "
+          "you can use to automatically expand the articles and comments. Type the rest of the command "
+          "you want to call, close the parenthesis and hit Enter.")
+    print()
+    print("E.g. you could type 'do_all()' and Enter to run all the other commands in sequence.")
+    print()
+    print("to exit (the interactive shell), press Ctrl-D")
+    print()
+    print("PS: In order for manual command execution to work, make sure you are running this script in interactive mode "
+          "by explicitly calling the 'python' command and passing in the '-i' flag. See the --help output for an example.")
+    print()
+
+###############################################################
+
+if(not args.manual):
+    nb_attempts = 6
+    for i in range(nb_attempts):
+        attempt_do_all(i, nb_attempts)
+
+print_manual_instructions()
+
+# if saving as PDF, remove fixed position CSS of search bar to prevent it from being printed on each page
