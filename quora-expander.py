a027b673cf3a0d664e41550c007432c91f5b4e32
ue70941 <christopher.dickinson@sbb.ch>
2022-01-14 12:12:37 +0200

    fix for show_more_*

--- quora-expander.py
+++ quora-expander.py
@@ -69,8 +69,7 @@ def click_on_all(browser, find_by, selector):
 
 
 def show_more_of_answers(browser):
-    # click_on_all(browser, browser.find_elements_by_xpath, "//div[contains(@class, 'spacing_log_answer_content')]//div[contains(text(), '(more)')]")
-    click_on_all(browser, browser.find_elements_by_xpath, "//div[contains(text(), '(more)')]") # same as show_more_of_comments
+    click_on_all(browser, browser.find_elements_by_xpath, "//div[contains(text(), '(more)') and ancestor::*[contains(@class, 'spacing_log_answer_content')]]")
 
 def view_more_comments(browser):
     click_on_all(browser, browser.find_elements_by_xpath, "//div[text()[contains(., 'View more comments')]]")
@@ -85,7 +84,7 @@ def view_more_replies(browser):
     click_on_all(browser, browser.find_elements_by_xpath, "//div[text()[contains(., 'View more replies')]]")
 
 def show_more_of_comments(browser):
-    click_on_all(browser, browser.find_elements_by_xpath, "//div[contains(text(), '(more)')]") # same as show_more_of_answers
+    click_on_all(browser, browser.find_elements_by_xpath, "//div[contains(text(), '(more)') and not(ancestor::*[contains(@class, 'spacing_log_answer_content')])]")
 
 def try_again(browser):
     click_on_all(browser, browser.find_elements_by_xpath, "//div[text()[contains(., 'Try again')]]")
